5977bcc7e12ca77f37ef25e7626d26653e41cf6c
01Qin <sisukasqin@hotmail.com>
2024-08-22 16:22:38 +0800

    second commit, variable and format

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum = num1 + num2
+sum = sum * 2
+print(sum)
+
+name = input("Enter your name: ")
+school = input("Enter your school: ")
+floatvarible = 1.2344566
+print("you are: ", name, "and your school is: ", school)
+print(f" your name is {name}, and your school is {school}")
+print(f" your name is {name}, and your school is {school}, and your float is {floatvarible: .2f}")
+
 invariable = 4
 floatvariable = 4.5
 stringvariable = "software 1 is fun"
@@ -7,3 +20,13 @@ print(floatvariable)
 print(stringvariable)
 
 print(type(invariable))
+
+#typecasting
+intvariable = int(floatvariable)
+print("here is the int version of float variable", intvariable)
+
+#division
+shareofloan = 500.50/3
+print(shareofloan)
+print(int(shareofloan))
+print(type(shareofloan))
