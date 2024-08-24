846f815b4ebf2f834ec8f6168d6639c7b77e9f39
01Qin <sisukasqin@hotmail.com>
2024-08-24 15:40:50 +0300

    second commit, variable and format

--- number.py
+++ number.py
@@ -21,7 +21,7 @@ length = float(input("Enter a height of a rectangle: "))
 width = float(input("Enter a width of a rectangle: "))
 area = length * width
 perimeter = 2 * (length + width)
-print(f"area is {area}, Perimeter is: {perimeter}.")
+print(f"The area is {area}, the perimeter is: {perimeter}.")
 
 #4
 a = int(input("Enter a number: "))
@@ -35,15 +35,15 @@ print(f"The average is: {average}, the sum is: {sum}")
 pounds_per_talent = 20
 lots_per_pound = 32
 grams_per_lot = 13.3
-talents = float(input("Enter number of talents: "))
-pounds = float(input("Enter number of pounds: "))
-lots = float(input("Enter number of lots: "))
+talents = float(input("Enter a number of talents: "))
+pounds = float(input("Enter a number of pounds: "))
+lots = float(input("Enter a number of lots: "))
 sum_pounds = pounds + talents * 20
 sum_lots = lots + sum_pounds * 32
 grams = float(13.3 * float(sum_lots))
 kilograms = float(grams / 1000)
 sum_grams = grams % 1000
-print(f"The weight in modern units: ")
+print(f"The weight in modern units are: ")
 print(f"{int(kilograms)} kilograms and {sum_grams:.2f} grams")
 
 #6
