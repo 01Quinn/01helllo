8cf71658f9dac9ed8cc37921963f056e0a49ed8f
01Qin <sisukasqin@hotmail.com>
2024-09-03 17:43:41 +0300

    while

--- while.py
+++ while.py
@@ -1,23 +1,20 @@
 #1
 i = 1
-while i < 1000:
+while i <= 1000:
     if i % 3 == 0:
         print(i)
     i += 1
 
 #2
-num = input("Enter a number: ")
+num = input(float("Enter a number of inches: "))
+num1 = float(num * 2.54)
 while True:
-    if num < 0:
-        break
-    num = float(input("Enter a number: "))
-    if num > 0:
-        num1 = float(num * 2.54)
-        print(num1)
-        num = float(input("Enter a number: "))
+    if num >= 0:
+        print(f"{num} inches equals to {num1}.")
+    num = float(input("Enter a number of inches: "))
     else:
-        break
-
+        print("Execution stopped.")
+    break
 
 #3
 max_num = 0
@@ -26,11 +23,11 @@ num1 = float(input("Enter a number: "))
 while num1 > 0:
     if num1 > max_num:
         max_num = num1
-        print(max_num)
+        print(num1)
     num1 = float(input("Enter a number: "))
     if num1 < min_num:
         min_num = num1
-        print(min_num)
+        print(num1)
     if num == " ":
         break
 
