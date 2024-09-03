dc9d7eae92a6d5b25ab47d77a3b32d1178d3325b
01Qin <sisukasqin@hotmail.com>
2024-09-03 15:13:04 +0300

    list exercise

--- list exercises.py
+++ list exercises.py
@@ -1,6 +1,3 @@
-from scipy.interpolate import insert
-from sympy.external.gmpy import remove
-
 # names = ["AB","BC","CD","DE","EF"]
 # id(names)
 # list1 = ["a", "b", "c", "d"]
@@ -8,16 +5,35 @@ from sympy.external.gmpy import remove
 # list2[3] = "k"
 # id(list1)
 # print(len(list1))
+#for number in range(1,11):
+    # print(number)
 
-names = []
-name = input("Enter names: ")
-while name != "":
-    names.append(name)
-    name = input("Enter the next name or quit by pressing Enter: ")
-    print(names)
-    if "t" in name:
-        names.remove("t")
-        names.insert(3, "o")
-    name = input("Enter the next name or quit by pressing Enter: ")
+# names = []
+# name = input(str("Enter names: "))
+# name1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# while name != "":
+#     names.append(name)
+#     name = input("Enter the next name or quit by pressing Enter: ")
+#     print(names)
+#     if "t" in name:
+#         names.remove('t')
+#         names.append(name)
+#         names.insert(3, "o")
+#         names.extend(name1)
+#         for n in names:
+#             print(f"hello,{n}")
+#
+# name = input("Enter the next name or quit by pressing Enter: ")
+# print(names)
 
-    print(names)
+# num = input("Enter a number: ")
+# for i in range(1,9,2):
+#     print(i)
+
+num = int(input("Enter a number: "))
+for i in range(1,num+1):
+    if num % 2 == 0:
+        print(f" the number {i} is even!")
+    else:
+        print(f" the number {i} is odd!")
+
