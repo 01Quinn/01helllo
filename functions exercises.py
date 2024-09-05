66a420e23f846e2ed2a7b53abd1e8ad54d37eaf9
01Qin <sisukasqin@hotmail.com>
2024-09-05 11:19:23 +0300

    functions

--- functions exercises.py
+++ functions exercises.py
@@ -14,26 +14,45 @@
 # print(cal)
 
 #3
-def sum(a, b):
-    sum = a + b
-    return sum
-print(sum(1,2))
-sum(1, 2)
+# def sum(a, b):
+#     sum = a + b
+#     return sum
+# print(sum(1,2))
+# sum(1, 2)
+#
+# def subtract(a, b):
+#     subtract = a - b
+#     if b != 0:
+#         return subtract
+# print(subtract(1,2))
+# subtract(2, 3)
+#
+#
+# def divide(a, b):
+#     divide = a / b
+#     if b != 0:
+#         return divide
+# print(divide(2, 3))
+# divide(2, 3)
+#
+#
+# def multiply(a, b):
+#     multiply = a * b
+#     return multiply
+# print(multiply(2, 3))
+# multiply(2, 3)
 
-def subtract(a, b):
-    if b != 0:
-        return a - b
-subtract(2, 3)
-
-
-def divide(a, b):
-    if b != 0:
-        return a / b
-divide(2, 3)
-
-
-def multiply(a, b):
-    return a * b
-multiply(2, 3)
-
+#4
+score = []
+def square(number):
+    return number * number
+ while True:
+    n = int(input('Enter a number: '))
+    if n == 0:
+        break
+    else:
+        score.append(n)
+print(f"{n} square :", square(n))
+print("here is the list:")
+print(score)
 
