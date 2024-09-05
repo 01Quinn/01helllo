b81b96867a386410d82fb6cc07a87e156761c8f7
01Qin <sisukasqin@hotmail.com>
2024-09-05 10:05:13 +0300

    while

---
+++ functions exercises.py
@@ -0,0 +1,14 @@
+#1
+def greetings():
+    a = 2
+    b = 3
+    sum = a + b
+    print(f"sum is {sum}")
+greetings()
+
+#2
+def multiply():
+    return 5 * 5
+print(multiply())
+cal = multiply() + 10
+print(cal)
