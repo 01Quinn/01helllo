02c4f8d8f77ca3e8f49ce528ab3303d6589f803e
01Qin <sisukasqin@hotmail.com>
2024-09-05 10:43:41 +0300

    functions

--- functions exercises.py
+++ functions exercises.py
@@ -1,31 +1,34 @@
 #1
-def greetings():
-    a = 2
-    b = 3
-    sum = a + b
-    print(f"sum is {sum}")
-greetings()
-
-#2
-def multiply():
-    return 5 * 5
-print(multiply())
-cal = multiply() + 10
-print(cal)
+# def greetings():
+#     a = 2
+#     b = 3
+#     sum = a + b
+#     print(f"sum is {sum}")
+# greetings()
+#
+# 2
+# def multiply():
+#     return 5 * 5
+# print(multiply())
+# cal = multiply() + 10
+# print(cal)
 
 #3
 def sum(a, b):
     sum = a + b
     return sum
+print(sum(1,2))
 sum(1, 2)
 
 def subtract(a, b):
-    return a - b
+    if b != 0:
+        return a - b
 subtract(2, 3)
 
 
 def divide(a, b):
-    return a / b
+    if b != 0:
+        return a / b
 divide(2, 3)
 
 
