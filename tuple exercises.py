2c904c7944fd0b71fc8d7e510a7f1314233e8ef7
01Qin <sisukasqin@hotmail.com>
2024-09-26 09:36:39 +0300

    functions.py

--- tuple exercises.py
+++ tuple exercises.py
@@ -4,7 +4,9 @@
 # day = tuple[day_num - 1]
 # print(f"the day {day_num} is {day}")
 
-num = ()
-enter_num = int(input("Enter a number: "))
+# num = ()
+# enter_num = int(input("Enter a number: "))
 
+player = ['c', 'b','d']
+print(player[-2:])
 
