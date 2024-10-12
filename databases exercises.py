78fd07178e779b9d4a98ac5243fb92bf947cdb74
01Qin <sisukasqin@hotmail.com>
2024-10-12 11:17:55 +0300

    database

--- databases exercises.py
+++ databases exercises.py
@@ -4,7 +4,7 @@
 # (town) from the airport database used on this course.
 # The ICAO codes are stored in the ident column of the airport table.
 import mysql.connector
-from geopy.distance import geodesic
+
 connection = mysql.connector.connect(
     host = '127.0.0.1',
     port = 3306,
@@ -14,10 +14,23 @@ connection = mysql.connector.connect(
     autocommit = True)
 print("connection")
 
-def get_airport_coordinates(icao_code):
-    sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident ='{icao_code}'"
+def get_airport(icao_code):
+    sql = f"SELECT name, lmunicipality FROM airport WHERE ident ='{icao_code}'"
     print(sql)
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
+
+
+def main():
+    while True:
+        icao_code = input("Enter the ICAO code of the airport or quit(q): ")
+
+        if icao_code.lower() == 'q':
+            print("Exiting the program...")
+            break
+
+
+
+main()
