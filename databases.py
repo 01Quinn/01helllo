e71e1d31e92aa11fffb4cb4d7db173ba6bc02564
01Qin <sisukasqin@hotmail.com>
2024-10-13 12:04:07 +0300

    database

--- databases.py
+++ databases.py
@@ -42,6 +42,51 @@ def main():
 
 main()
 
+
+# 2
+import mysql.connector
+connection = mysql.connector.connect(
+    host = '127.0.0.1',
+    port = 3306,
+    database = 'airports',
+    user = 'root',
+    password = '453489',
+    autocommit = True)
+print("Connection established.")
+
+
+def get_airport(area_code):
+    sql = (f"SELECT name, type "
+           f"FROM airport WHERE iso_country = %s "
+           f" order by type")
+
+    cursor = connection.cursor()
+    cursor.execute(sql, (area_code,))
+    result = cursor.fetchall()
+    return result
+
+
+def main():
+    while True:
+        area_code = input("Enter the area code (e.g., FI) or 'q' to quit: ")
+        if area_code == 'q':
+            print("Bye")
+            break
+
+        airport_info = get_airport(area_code)
+
+        if airport_info:
+
+            print(f"Airport in {area_code}:")
+            for airport_type, name in airport_info:
+                print(f"\t{airport_type}: {name}")
+        else:
+            print("No airports found.")
+
+
+main()
+
+
 # 3
 import mysql.connector
 from geopy.distance import geodesic
