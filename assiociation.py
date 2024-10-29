d2ec63c5350ea515205c3cc3d966b95bb7e7445f
01Qin <sisukasqin@hotmail.com>
2024-10-29 11:24:22 +0200

    association

--- assiociation.py
+++ assiociation.py
@@ -1,4 +1,25 @@
 # 1
 class Elevator:
 
-    def __init__(self):
+    def __init__(self, num_top, num_bottom):
+        self.num_top = num_top
+        self.num_bottom = num_bottom
+        self.new_elevator = num_bottom
+
+    def go_to_floor(self,floor):
+        if floor < self.num_bottom or floor > self.num_top:
+            print("Error!")
+            return
+        if floor > self.new_elevator:
+            self.floor_up(floor)
+        elif floor < self.new_elevator:
+            self.floor_down(floor)
+        else:
+            print(f"The elevator is on the floor{floor}")
+
+
+    def floor_up(self, floor):
+
+
+
+    def floor_down(self, floor):
