dea8230176c01d39d8d897a0c9bf8a2e20d3ddd1
01Qin <sisukasqin@hotmail.com>
2024-10-29 13:29:58 +0200

    association

--- assiociation exercises.py
+++ assiociation exercises.py
@@ -46,6 +46,7 @@
 #     employee.print_information()
 #
 from jedi.debug import speed
+from requests.packages import target
 
 
 # class Vehicle:
@@ -74,65 +75,98 @@ from jedi.debug import speed
 # bike = Bicycle
 # print(bike.is_sport_item(SportsItem()))
 
-class Storage:
-    def __init__(self):
-        self.file = []
+# class Storage:
+#     def __init__(self):
+#         self.file = []
+#
+#     def add_file(self, file):
+#         self.file.append(file)
+#
+#     def list_files_in_storage(self):
+#         for file in self.file:
+#             print(file)
+#
+# class Camera:
+#     def take_photo(self):
+#         print('Taking photo')
+#         self.photo = "my photo"
+#
+# class Gps:
+#     lat = 61
+#     lon = 20
+#
+# class Call:
+#     def dial(self, to_number):
+#         for number in to_number:
+#             print(f'Dialing {to_number}')
+#
+#     def make_call(self, to_number):
+#         self.dial(to_number)
+#         print(f'Calling {to_number}')
+#
+# class Phone:
+#     def __init__(self, brand, model,price):
+#         storage.__init__(self)
+#         self.brand = brand
+#         self.price = price
+#         self.model = model
+#
+#     def print_details(self):
+#         print(self.brand, self.model, self.price)
+#
+# class Laptop(Storage, Camera):
+#     def __init__(self):
+#         Storage.__init__(self)
+#         Camera.__init__(self)
+#
+#         print("laptop")
+#
+#
+# phone1 = Phone('Bob', 100)
+# phone2 = Phone('Alice', 100)
+# phone3 = Phone('Bob', 100)
+# phone.print_details()
+# phone.add_file
+# phone.take_photo
+# print(phone.photo)
+# phone.make
+# print(phone.lat)
+# print(phone.lon)
+#
+#
+#
+# laptop = Laptop()
+# laptop.add_file(excel.xlx)
 
-    def add_file(self, file):
-        self.file.append(file)
+class Elevator:
 
-    def list_files_in_storage(self):
-        for file in self.file:
-            print(file)
+    def __init__(self, num_top, num_bottom):
+        self.num_top = num_top
+        self.num_bottom = num_bottom
+        self.new_elevator = num_bottom
 
-class Camera:
-    def take_photo(self):
-        print('Taking photo')
-        self.photo = "my photo"
+    def go_to_floor(self, floor):
+        if floor < self.num_bottom or floor > self.num_top:
+            print("Error!")
+            return
+        if floor > self.new_elevator:
+            self.floor_up(floor)
+        elif floor < self.new_elevator:
+            self.floor_down(floor)
+        else:
+            print(f"The elevator is on {floor}")
 
-class Gps:
-    lat = 61
-    lon = 20
+    def floor_up(self, floor):
+        while self.new_elevator < floor:
+            self.new_elevator += 1
+            print(f"The elevator is moving from {floor}to the floor{self.new_elevator}")
 
-class Call:
-    def dial(self, to_number):
-        for number in to_number:
-            print(f'Dialing {to_number}')
-
-    def make_call(self, to_number):
-        self.dial(to_number)
-        print(f'Calling {to_number}')
-
-class Phone:
-    def __init__(self, brand, model,price):
-        storage.__init__(self)
-        self.brand = brand
-        self.price = price
-        self.model = model
-
-    def print_details(self):
-        print(self.brand, self.model, self.price)
-
-class Laptop(Storage, Camera):
-    def __init__(self):
-        Storage.__init__(self)
-        Camera.__init__(self)
-
-        print("laptop")
+    def floor_down(self, floor):
+        while self.new_elevator > floor:
+            self.new_elevator -= 1
+            print(f"The elevator is moving from {floor}to {self.new_elevator}")
 
 
-phone1 = Phone('Bob', 100)
-phone2 = Phone('Alice', 100)
-phone3 = Phone('Bob', 100)
-phone.print_details()
-phone.add_file
-phone.take_photo
-print(phone.photo)
-phone.make
-print(phone.lat)
-print(phone.lon)
-
-
-
-laptop = Laptop()
-laptop.add_file(excel.xlx)
+h = Elevator(6, 1)
+target_floor = int(input("Enter the floor: "))
+h.go_to_floor(target_floor)
--- assiociation.py
+++ assiociation.py
@@ -6,7 +6,7 @@ class Elevator:
         self.num_bottom = num_bottom
         self.new_elevator = num_bottom
 
-    def go_to_floor(self,floor):
+    def go_to_floor(self, floor):
         if floor < self.num_bottom or floor > self.num_top:
             print("Error!")
             return
@@ -17,54 +17,43 @@ class Elevator:
         else:
             print(f"The elevator is on {floor}")
 
-
     def floor_up(self, floor):
         while self.new_elevator < floor:
             self.new_elevator += 1
             print(f"The elevator is moving from {floor}to the floor{self.new_elevator}")
-
 
     def floor_down(self, floor):
         while self.new_elevator > floor:
             self.new_elevator -= 1
             print(f"The elevator is moving from {floor}to {self.new_elevator}")
 
+
+h = Elevator(6, 1)
+target_floor = int(input("Enter the floor: "))
+h.go_to_floor(target_floor)
+
 # 2
-class Elevator:
-
-    def __init__(self, num_top, num_bottom):
-        self.num_top = num_top
-        self.num_bottom = num_bottom
-        self.new_elevator = num_bottom
-
-    def go_to_floor(self,floor):
-        if floor < self.num_bottom or floor > self.num_top:
-            print("Error!")
-            return
-        if floor > self.new_elevator:
-            self.floor_up(floor)
-        elif floor < self.new_elevator:
-            self.floor_down(floor)
-        else:
-            print(f"The elevator is on the floor{floor}")
-
-
-    def floor_up(self, floor):
-        while self.new_elevator < floor:
-            self.new_elevator += 1
-            print(f"The elevator is on the floor{floor}")
-
-
-    def floor_down(self, floor):
-        while self.new_elevator > floor:
-            self.new_elevator -= 1
-            print(f"The elevator is on the floor{floor}")
 
 class Building:
-    def __init__(self, floor):
-        self.floor = floor
+    def __init__(self, num_bottom, num_top, num_elevators):
+        self.num_bottom = num_bottom
+        self.num_top = num_top
+        self.num_elevators = num_elevators
+        self.num_elevators = []
+
+        for e in range(num_elevators):
+            elevator = Elevator(num_bottom, num_top)
+            self.num_elevators.append(elevator)
+
+    def run_elevator(self, number_elevator, destination):
+        if number_elevator < 0 or number_elevator >= self.num_elevators:
+            print("Error!")
+            return
+
+        elevator = self.elevators[number_elevator]
+        print(f"The elevator {number_elevator} goes to floor {destination}.")
+        elevator.go_to_floor(destination)
 
 
-    def run_elevator(self, number_of_elevators, destination_floor):
-
-elevators = []
+building = Building(1, 10, 7)
+building.run_elevator(1, 8)
